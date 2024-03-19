8501f6925efd72a3a55a43f9d73306b2dbf7a656
Dammydrizzle <Dammydrizzle@gmail.com>
2024-03-19 15:24:03 +0100

    added the OAuth functionality

--- my realtor app/src/firebase.js
+++ my realtor app/src/firebase.js
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDJEiX5GbvstrPQgG_ePKdIuAUiIQzKN1c",
   authDomain: "realtor-clone-394a8.firebaseapp.com",
@@ -13,6 +15,7 @@ const firebaseConfig = {
   messagingSenderId: "643683888140",
   appId: "1:643683888140:web:c470edba62318237855229"
 };
+  
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
