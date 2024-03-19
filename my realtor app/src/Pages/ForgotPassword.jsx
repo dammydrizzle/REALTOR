fc2e4849c0d0dba93bdd3916689e4ffb239a0059
Dammydrizzle <Dammydrizzle@gmail.com>
2024-03-19 17:07:40 +0100

    complete the forgotpassword functionality

--- my realtor app/src/Pages/ForgotPassword.jsx
+++ my realtor app/src/Pages/ForgotPassword.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 import OAuth from '../components/OAuth'
+import { toast } from 'react-toastify'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("")
   function onChange(e) {
       setEmail(e.target.value)
       
+  }
+  async function onSubmit (e) {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("email was sent")
+    } catch (error) {
+      toast.error("Could not send reset password")
+    }
   }
   return (
     <section>
@@ -18,7 +30,7 @@ export default function ForgotPassword() {
           <img src="https://images.unsplash.com/photo-1609770231080-e321deccc34c?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="key" className="w-full rounded-2xl" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" type="email" id="email" value={email} onChange={onChange} placeholder="Email address" />
             
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
